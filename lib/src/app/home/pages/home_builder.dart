f67e89c52d0f80beab9b2a4a4a75d1530a9f8fe1
fagnerdossantos <61123877+fagnerdossantos@users.noreply.github.com>
2023-04-05 15:01:31 -0300

    Version changes

--- lib/src/app/home/pages/home_builder.dart
+++ lib/src/app/home/pages/home_builder.dart
@@ -4,15 +4,26 @@ import 'package:coins/src/app/load/pages/load_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeBuilder extends StatelessWidget {
+class HomeBuilder extends StatefulWidget {
   const HomeBuilder({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final blocController = BlocProvider.of<CoinsBloc>(context);
+  State<HomeBuilder> createState() => _HomeBuilderState();
+}
 
+class _HomeBuilderState extends State<HomeBuilder> {
+  late final CoinsBloc blocController;
+
+  @override
+  void didChangeDependencies() {
+    blocController = BlocProvider.of<CoinsBloc>(context);
     blocController.add(FetchCoinEvent());
 
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return BlocBuilder<CoinsBloc, CoinsBlocState>(
       builder: (context, state) {
         // Load
@@ -21,8 +32,7 @@ class HomeBuilder extends StatelessWidget {
           return const LoadPage();
 
           // Success or Filtered
-        } else if (state is CoinsSuccessState ||
-            state is CoinsFilteredState) {
+        } else if (state is CoinsSuccessState || state is CoinsFilteredState) {
           return HomePage(
             state: state,
           );
